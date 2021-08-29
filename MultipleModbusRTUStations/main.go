b4c1b1457bd8e91819edca2f6d471cc66b967a63
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-08-29 20:49:09 +0530

    ConnectToRTU has intreface to connect to RTU Clients.

--- MultipleModbusRTUStations/main.go
+++ MultipleModbusRTUStations/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	NewRTUSingleStation()
+
+	// Connect according to choice
+	err := ConnectToRTU()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ConnectToRTU() (err error) {
+	var choice string
+
+	fmt.Println(`Enter Your choice:
+	1. Connect to single station.
+	2. Connect to 2 stations- The wrong way.
+	3. Connect to 2 stations- The right way.`)
+
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case "1":
+		_, err = ConnectRTUSingleStation(station_1_ID, nil)
+	case "2":
+		err = ConnectRTUMultipleStations_WrongWay()
+	case "3":
+		err = ConnectRTUMultipleStations_RightWay()
+	default:
+		return fmt.Errorf("please verify your choice")
+	}
+	return
 }
